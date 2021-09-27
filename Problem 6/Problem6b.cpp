880c9d97fd0054df6188ba35a03a67a06353f589
JulianElToro <89639454+JulianElToro@users.noreply.github.com>
2021-09-27 19:40:14 +0200

    Update Problem6b.cpp

--- Problem 6/Problem6b.cpp
+++ Problem 6/Problem6b.cpp
@@ -1,8 +1,10 @@
 #include "Problem5a-2.hpp"
 
 int main(){
-
+    
     double eps = pow(10, -8);
+    
+    //We can use ofstream ofile to create a .txt file to save the number of iterations and the size of the matrix
 
     ofstream ofile;
     ofile.open("iterations_densematrix.txt");
@@ -12,15 +14,15 @@ int main(){
     
     for (int N = 2; N <= 100; N++) {
 
-        //We define h , which is the stepsize
-
-        double h = 1. / (N + 1);
-
-        //Now we define the matrix A full of zeros
+        //Now we define the matrix A full of random values
 
         mat Ap(N, N, fill::randn);
+        
+        //But symmetric
 
         mat A = symmatu(Ap);
+        
+        //We define some variables we need for the jacobi_eigensolver function
 
         vec eigenvalues;
         mat eigenvectors;
