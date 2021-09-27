e17b56b014bcc8b2bc2a7d341bb4ee84bf606b17
JulianElToro <89639454+JulianElToro@users.noreply.github.com>
2021-09-27 19:42:19 +0200

    Update Problem6b.py

--- Problem 6/Problem6b.py
+++ Problem 6/Problem6b.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Sep 27 00:30:48 2021
-
-@author: julia
-"""
-
 import numpy as np
 import matplotlib.pyplot as plt
    
@@ -17,7 +10,7 @@ plt.figure(figsize=(figwidth, figheight))
 
 textname = 'iterations_densematrix.txt' 
 
-
+# A loop will read the data and save it in two vectors
 
 for i in range(0,1):
     N1 = []
@@ -61,13 +54,18 @@ for i in range(0,1):
     N = np.array(N1)
     
     it = np.array(it1)
-    
-    plt.plot(N, it , linewidth = 0.5, color = 'pink' )
-    plt.plot(N, it , '.' , color = 'red' , label = 'Triangular')
+      
+# Let's plot the results
+
+plt.plot(N, it , linewidth = 0.5, color = 'pink' )
+plt.plot(N, it , '.' , color = 'red' , label = 'Triangular')
     
 plt.title("Number of required transformations vs the matrix size N", fontsize=10)
 plt.legend()
 plt.ylabel('Number of transformations')
 plt.xlabel("N")
 plt.grid(True) #Grids get painted
+
+# The graph is saved in a .pdf file
+
 plt.savefig("Problem6b_plot.pdf")
