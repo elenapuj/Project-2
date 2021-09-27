bfa87ce423cd6f806ead9e56339ebea33eeb686b
ivanvillegas7 <89591182+ivanvillegas7@users.noreply.github.com>
2021-09-27 19:18:57 +0200

    Update Problem5a-1.hpp

--- Problem5a/Problem5a-1.hpp
+++ Problem5a/Problem5a-1.hpp
@@ -1,14 +1,20 @@
 #include "Problem4a.hpp"
 
+// A function is created to do the Jacobi rotation
+
 void jacobi_rotate(mat& A, mat& R, int& k, int& l) {
 
     int N = A.n_rows;
+    
+    //The maximum value we are looking for is obtained
     
     double value = max_offdiag_symmetric(A, k, l);
 
     double c, s;
 
     if (A(k,l) != 0.0) {
+        
+        //Tau is defined and Tan(θ) is declared; it will be defined in the following if loop
         
         double tau = (A(l, l) - A(k, k)) / (2. * A(k, l));
         double t;
@@ -24,6 +30,8 @@ void jacobi_rotate(mat& A, mat& R, int& k, int& l) {
         
         }
         
+        //Cos(θ) and Sin(θ) are defined as a function of Tan(θ)
+        
         c = 1. / sqrt(1. + t * t);
         
         s = c * t;
@@ -33,7 +41,9 @@ void jacobi_rotate(mat& A, mat& R, int& k, int& l) {
         s = 0.0;
     }
 
-
+    
+    //Elements A(l, l) and A(k, k) are stored in memory
+    
     double akk;
 
     double all;
@@ -41,7 +51,9 @@ void jacobi_rotate(mat& A, mat& R, int& k, int& l) {
     akk = A(k, k) * c * c - 2. * A(k, l) * c * s + A(l, l) * s * s;
 
     all = A(l, l) * c * c + 2. * A(k, l) * c * s + A(k, k) * s * s;
-
+    
+    //Elements A(k, k), A(l, l), A(k, l) and A(l, k) are defined
+    
     A(k,k) = akk;
 
     A(l,l) = all;
@@ -55,7 +67,9 @@ void jacobi_rotate(mat& A, mat& R, int& k, int& l) {
     for (int j = 0; j < N; j++) {
 
         if (j != k && j != l) {
-
+            
+            //Every element (except for the ones in the diagonal) of A is redefined 
+            
             double ak, al;
 
             ak = A(j,k);
@@ -65,6 +79,8 @@ void jacobi_rotate(mat& A, mat& R, int& k, int& l) {
             A(j,l) = c * al + s * ak;
             A(l,j) = A(j,l);
         }
+        
+        //Every element of matrix R is defined
         
         double rk, rl;
         
