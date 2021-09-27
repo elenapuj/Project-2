727534572da3c359c44da14eb245576508549f79
ivanvillegas7 <89591182+ivanvillegas7@users.noreply.github.com>
2021-09-27 19:08:16 +0200

    Update Problem4b.cpp

--- Problem4/Problem4b.cpp
+++ Problem4/Problem4b.cpp
@@ -1,14 +1,20 @@
 #include "Problem4a.hpp"
 int main() {
 
+    //Matrix A is created (declared) and defined
+    
     mat A;
     A << 1. << 0. << 0. << 0.5 << endr
         << 0. << 1. << -0.7 << 0. << endr
         << 0. << -0.7 << 1. << 0. << endr
         << 0.5 << 0. << 0. << 1. << endr;
-
+    
+    //k and l index are declared
+    
     int k, l;
-
+    
+    //The maximum value we are loking for is obteined and printed with it's correspondent index
+    
     double value = max_offdiag_symmetric(A, k, l);
 
     cout << k << " " << l << " " << value;
